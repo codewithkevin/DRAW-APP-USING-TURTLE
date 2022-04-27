074c45c6044a6ca53b4ab4d9e5fd56eab36a275e
codewithkevin <oseikelvin709@gmail.com>
2022-04-27 14:46:17 +0000

    update

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-from turtle import *
-
-from draw import *
-
-screen = Screen()
-screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.title("DRAWING GAME")
-
-
-'''OOP CODES GOES HERE'''
-tim = Tim(screen)
-tim.check()
-
-
-
-screen.exitonclick()
+name = input("What is your name? ").upper()
+age = int(input("How old are you? "))
 
