c0653ee62a2f9e9f309f38b02d13f88e43dc8aa2
codewithkevin <oseikelvin709@gmail.com>
2022-04-28 14:38:17 +0000

    update

--- draw.py
+++ draw.py
@@ -1,25 +1,25 @@
 from turtle import Turtle
 
-SQUARE = 4
 TRIANGLE = 3
-CIRCLE = 2
+SQUARE = 4
 
-DRAW_LIST = ['triangle', 'square', 'circle']
-EXIST_LIST = ['stop', 'n', 'nope', 'exist']
+DRAW_LIST = ['triangle', 'square']
+EXIST_LIST = ['stop', 'n', 'nope', 'exit']
 
 
 class Tim(Turtle):
 
     def __init__(self, display):
         super().__init__()
-        self.display = display
         self.should_continue = True
-        self.shape("turtle")
-        self.color("white")
-        self.goto(-40, 0)
+        self.shape('square')
+        self.color('white')
+        self.penup()
+        self.goto(x=-40, y=0)
+        self.display = display
         self.choice = self.display.textinput(title="WHAT DOYOU WANT TO DRAW", prompt='Enter your response;\n')
 
-    def  check(self):
+    def checking_answer(self):
         while self.should_continue:
             def game(number_of_side):
                 angle = 360 / number_of_side
@@ -27,18 +27,12 @@ class Tim(Turtle):
                     self.forward(100)
                     self.left(angle)
 
-            for shape in DRAW_LIST:
-                if self.choice == shape:
-                    if shape == 'triangle':
-                        game(TRIANGLE)
-                        break
-                        
-                    elif shape == 'square':
-                        game(SQUARE)
-                        break
-                         
-                    elif shape == 'circle':
-                        game(CIRCLE)
-                        break
-                    
-                
+            if self.choice == 'triangle':
+                self.pendown()
+                game(TRIANGLE)
+                break
+
+            elif self.choice.lower() == 'square':
+                self.pendown()
+                game(SQUARE)
+                break
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from turtle import *
-from draw import *
+from turtle import Screen
+from draw import Tim
 
+restart = True
 
 screen = Screen()
-screen.bgcolor("black")
-screen.title("Turtle")
+screen.bgcolor('black')
+screen.setup(width=600, height=600)
+screen.title('DRAW APP')
 
+
+"""ALL OOP CODES GOES IN HERE """
 tim = Tim(screen)
 
-tim.check()
+
+tim.checking_answer()
 
 
 
-
-screen.existonclick()
+screen.exitonclick()
